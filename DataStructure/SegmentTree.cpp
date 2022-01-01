07cdd23fb21cafcb1e70be45a95c4d6f306a107d
kaichou243 <83384006+kaichou243@users.noreply.github.com>
2022-01-01 21:48:01 +0900

    Update SegmentTree.cpp

--- DataStructure/SegmentTree.cpp
+++ DataStructure/SegmentTree.cpp
@@ -1,56 +1,61 @@
-/*
-注意
-このセグ木は使わないことを強くお勧めします。
-*/
 #include<bits/stdc++.h>
 using namespace std;
 using ll=long long;
 using P=pair<ll,ll>;
-struct SegmentTree{
-  ll g(int a,int b){
-    return a+b; //g(a,b) : aとbで二項演算を行った結果を返す関数
-  }
+template<typename T> struct SegmentTree{
+  using F=function<T(T,T)>;
+  F fT;
+  const T et;
   int n;
-  vector<ll> dat;
-  SegmentTree(int N){
+  vector<T> dat;
+  SegmentTree(int N,F fT_,T et_) : fT(fT_),et(et_){
     n=1;
     while(n<N)n*=2;
-    dat.assign(2*n-1,0);
+    dat.assign(2*n-1,et_);
   }
-  //加算クエリ
-  void add(int k,ll x){
+  void add(int k,T x){
     k+=n-1;
     dat[k]+=x;
     while(k){
       k=(k-1)/2;
-      dat[k]=g(dat[k*2+1],dat[k*2+2]);
+      dat[k]=fT(dat[k*2+1],dat[k*2+2]);
     }
   }
-  //更新クエリ
-  void update(int k,ll x){
+  void apply(int k,T x){
+    k+=n-1;
+    dat[k]=fT(dat[k],x);
+    while(k){
+      k=(k-1)/2;
+      dat[k]=fT(dat[k*2+1],dat[k*2+2]);
+    }
+  }
+  void update(int k,T x){
     k+=n-1;
     dat[k]=x;
     while(k){
       k=(k-1)/2;
-      dat[k]=g(dat[k*2+1],dat[k*2+2]);
+      dat[k]=fT(dat[k*2+1],dat[k*2+2]);
     }
   }
-  ll querry(int l,int r){
-    return querry_sub(l,r,0,0,n); //区間[l,r)に対する二項演算の総積を返す
+  T query(int l,int r){
+    return query_sub(l,r,0,0,n);
   }
-  ll querry_sub(int l,int r,int k=0,int a=0,int b=-1){
+  T query_sub(int l,int r,int k=0,int a=0,int b=-1){
     if(b<0)b=n;
-    if(r<=a || b<=l)return 0; //使う二項演算に合わせて単位元を変える。要注意!
+    if(r<=a || b<=l)return et;
     if(l<=a && b<=r)return dat[k];
-    return g(querry_sub(l,r,k*2+1,a,(a+b)/2),querry_sub(l,r,k*2+2,(a+b)/2,b));
+    return fT(query_sub(l,r,k*2+1,a,(a+b)/2),query_sub(l,r,k*2+2,(a+b)/2,b));
   }
 };
 //example
 //0-indexed
+//PAQ+RSQ
+auto f = [](ll x1,ll x2) -> ll { return x1+x2; };
+const ll e=0;
 int main(){
   int N,Q;
   cin>>N>>Q;
-  SegmentTree st(N);
+  SegmentTree<ll> st(N,f,e);
   for(int i=0;i<N;i++){
     int a;
     cin>>a;
@@ -60,15 +65,16 @@ int main(){
     int c;
     cin>>c;
     if(c==1){
-      int k,x;
+      int k;
+      ll x;
       cin>>k>>x;
-      //加算クエリ a_kにxを加算
+      //add a_k x
       st.add(k,x);
     }else if(c==2){
       int l,r;
       cin>>l>>r;
-      //区間[l,r)のクエリを返す この場合は[l,r)の総和
-      cout<<st.querry(l,r)<<endl;
+      //return product of [l,r)
+      cout<<st.query(l,r)<<endl;
     }
   }
 }

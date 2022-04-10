733d7b0021447adce95629fcd3c071d780a7aa72
kaichou243 <83384006+kaichou243@users.noreply.github.com>
2022-04-11 03:26:12 +0900

    Create normal_template.cpp

---
+++ normal_template.cpp
@@ -0,0 +1,412 @@
+#include<bits/stdc++.h>
+#pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#define FOR(i,n) for(int i = 0; i < (n); i++)
+#define sz(c) ((int)(c).size())
+#define ten(x) ((int)1e##x)
+#define all(v) (v).begin(), (v).end()
+using namespace std;
+using ll=long long;
+using P = pair<ll,ll>;
+const long double PI=acos(-1);
+const ll INF=1e18;
+const int inf=1e9;
+template<int MOD> struct Fp{
+  ll val;
+  constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+    if (val < 0) val += MOD;
+  }
+  static constexpr int getmod() { return MOD; }
+  constexpr Fp operator - () const noexcept {
+    return val ? MOD - val : 0;
+  }
+  constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
+  constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
+  constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
+  constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
+  constexpr Fp& operator += (const Fp& r) noexcept {
+    val += r.val;
+    if (val >= MOD) val -= MOD;
+    return *this;
+  }
+  constexpr Fp& operator -= (const Fp& r) noexcept {
+    val -= r.val;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr Fp& operator *= (const Fp& r) noexcept {
+    val = val * r.val % MOD;
+    return *this;
+  }
+  constexpr Fp& operator /= (const Fp& r) noexcept {
+    ll a = r.val, b = MOD, u = 1, v = 0;
+    while (b) {
+      ll t = a / b;
+      a -= t * b, swap(a, b);
+      u -= t * v, swap(u, v);
+    }
+    val = val * u % MOD;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr bool operator == (const Fp& r) const noexcept {
+    return this->val == r.val;
+  }
+  constexpr bool operator != (const Fp& r) const noexcept {
+    return this->val != r.val;
+  }
+  constexpr bool operator < (const Fp& r) const noexcept {
+    return this->val < r.val;
+  }
+  friend constexpr istream& operator >> (istream& is, Fp<MOD>& x) noexcept {
+    is >> x.val;
+    x.val %= MOD;
+    if (x.val < 0) x.val += MOD;
+    return is;
+  }
+  friend constexpr ostream& operator << (ostream& os, const Fp<MOD>& x) noexcept {
+    return os << x.val;
+  }
+  friend constexpr Fp<MOD> modpow(const Fp<MOD>& a, long long n) noexcept {
+    Fp<MOD> res=1,r=a;
+    while(n){
+      if(n&1) res*=r;
+      r*=r;
+      n>>=1;
+    }
+    return res;
+  }
+  friend constexpr Fp<MOD> modinv(const Fp<MOD>& r) noexcept {
+        long long a = r.val, b = MOD, u = 1, v = 0;
+        while (b) {
+            long long t = a / b;
+            a -= t * b, swap(a, b);
+            u -= t * v, swap(u, v);
+        }
+        return Fp<MOD>(u);
+  }
+  explicit operator bool()const{
+		return val;
+  }
+};
+//fast Input by yosupo
+#include <unistd.h>
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cctype>
+#include <cstring>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <vector>
+namespace fastio{
+/*
+  quote from yosupo's submission in Library Checker
+*/
+int bsr(unsigned int n) {
+    return 8 * (int)sizeof(unsigned int) - 1 - __builtin_clz(n);
+}
+// @param n `1 <= n`
+// @return maximum non-negative `x` s.t. `(n & (1 << x)) != 0`
+int bsr(unsigned long n) {
+    return 8 * (int)sizeof(unsigned long) - 1 - __builtin_clzl(n);
+}
+// @param n `1 <= n`
+// @return maximum non-negative `x` s.t. `(n & (1 << x)) != 0`
+int bsr(unsigned long long n) {
+    return 8 * (int)sizeof(unsigned long long) - 1 - __builtin_clzll(n);
+}
+// @param n `1 <= n`
+// @return minimum non-negative `x` s.t. `(n & (1 << x)) != 0`
+int bsr(unsigned __int128 n) {
+    unsigned long long low = (unsigned long long)(n);
+    unsigned long long high = (unsigned long long)(n >> 64);
+    return high ? 127 - __builtin_clzll(high) : 63 - __builtin_ctzll(low);
+}
+ 
+namespace internal {
+ 
+template <class T>
+using is_signed_int128 =
+    typename std::conditional<std::is_same<T, __int128_t>::value ||
+                                  std::is_same<T, __int128>::value,
+                              std::true_type,
+                              std::false_type>::type;
+ 
+template <class T>
+using is_unsigned_int128 =
+    typename std::conditional<std::is_same<T, __uint128_t>::value ||
+                                  std::is_same<T, unsigned __int128>::value,
+                              std::true_type,
+                              std::false_type>::type;
+ 
+template <class T>
+using make_unsigned_int128 =
+    typename std::conditional<std::is_same<T, __int128_t>::value,
+                              __uint128_t,
+                              unsigned __int128>;
+ 
+template <class T>
+using is_integral =
+    typename std::conditional<std::is_integral<T>::value ||
+                                  internal::is_signed_int128<T>::value ||
+                                  internal::is_unsigned_int128<T>::value,
+                              std::true_type,
+                              std::false_type>::type;
+ 
+template <class T>
+using is_signed_int = typename std::conditional<(is_integral<T>::value &&
+                                                 std::is_signed<T>::value) ||
+                                                    is_signed_int128<T>::value,
+                                                std::true_type,
+                                                std::false_type>::type;
+ 
+template <class T>
+using is_unsigned_int =
+    typename std::conditional<(is_integral<T>::value &&
+                               std::is_unsigned<T>::value) ||
+                                  is_unsigned_int128<T>::value,
+                              std::true_type,
+                              std::false_type>::type;
+ 
+template <class T>
+using to_unsigned = typename std::conditional<
+    is_signed_int128<T>::value,
+    make_unsigned_int128<T>,
+    typename std::conditional<std::is_signed<T>::value,
+                              std::make_unsigned<T>,
+                              std::common_type<T>>::type>::type;
+ 
+template <class T>
+using is_integral_t = std::enable_if_t<is_integral<T>::value>;
+ 
+template <class T>
+using is_signed_int_t = std::enable_if_t<is_signed_int<T>::value>;
+ 
+template <class T>
+using is_unsigned_int_t = std::enable_if_t<is_unsigned_int<T>::value>;
+ 
+template <class T> using to_unsigned_t = typename to_unsigned<T>::type;
+ 
+}  // namespace internal
+struct Scanner {
+  public:
+    Scanner(const Scanner&) = delete;
+    Scanner& operator=(const Scanner&) = delete;
+ 
+    Scanner(FILE* fp) : fd(fileno(fp)) {}
+ 
+    void read() {}
+    template <class H, class... T> void read(H& h, T&... t) {
+        bool f = read_single(h);
+        assert(f);
+        read(t...);
+    }
+ 
+    int read_unsafe() { return 0; }
+    template <class H, class... T> int read_unsafe(H& h, T&... t) {
+        bool f = read_single(h);
+        if (!f) return 0;
+        return 1 + read_unsafe(t...);
+    }
+ 
+    int close() { return ::close(fd); }
+ 
+  private:
+    static constexpr int SIZE = 1 << 15;
+ 
+    int fd = -1;
+    std::array<char, SIZE + 1> line;
+    int st = 0, ed = 0;
+    bool eof = false;
+ 
+    bool read_single(std::string& ref) {
+        if (!skip_space()) return false;
+        ref = "";
+        while (true) {
+            char c = top();
+            if (c <= ' ') break;
+            ref += c;
+            st++;
+        }
+        return true;
+    }
+    bool read_single(double& ref) {
+        std::string s;
+        if (!read_single(s)) return false;
+        ref = std::stod(s);
+        return true;
+    }
+ 
+    template <class T,
+              std::enable_if_t<std::is_same<T, char>::value>* = nullptr>
+    bool read_single(T& ref) {
+        if (!skip_space<50>()) return false;
+        ref = top();
+        st++;
+        return true;
+    }
+ 
+    template <class T,
+              internal::is_signed_int_t<T>* = nullptr,
+              std::enable_if_t<!std::is_same<T, char>::value>* = nullptr>
+    bool read_single(T& sref) {
+        using U = internal::to_unsigned_t<T>;
+        if (!skip_space<50>()) return false;
+        bool neg = false;
+        if (line[st] == '-') {
+            neg = true;
+            st++;
+        }
+        U ref = 0;
+        do {
+            ref = 10 * ref + (line[st++] & 0x0f);
+        } while (line[st] >= '0');
+        sref = neg ? -ref : ref;
+        return true;
+    }
+    template <class U,
+              internal::is_unsigned_int_t<U>* = nullptr,
+              std::enable_if_t<!std::is_same<U, char>::value>* = nullptr>
+    bool read_single(U& ref) {
+        if (!skip_space<50>()) return false;
+        ref = 0;
+        do {
+            ref = 10 * ref + (line[st++] & 0x0f);
+        } while (line[st] >= '0');
+        return true;
+    }
+ 
+    bool reread() {
+        if (ed - st >= 50) return true;
+        if (st > SIZE / 2) {
+            std::memmove(line.data(), line.data() + st, ed - st);
+            ed -= st;
+            st = 0;
+        }
+        if (eof) return false;
+        auto u = ::read(fd, line.data() + ed, SIZE - ed);
+        if (u == 0) {
+            eof = true;
+            line[ed] = '\0';
+            u = 1;
+        }
+        ed += int(u);
+        line[ed] = char(127);
+        return true;
+    }
+ 
+    char top() {
+        if (st == ed) {
+            bool f = reread();
+            assert(f);
+        }
+        return line[st];
+    }
+ 
+    template <int TOKEN_LEN = 0>
+    bool skip_space() {
+        while (true) {
+            while (line[st] <= ' ') st++;   
+            if (ed - st > TOKEN_LEN) return true;
+            if (st > ed) st = ed;
+            for (auto i = st; i < ed; i++) {
+                if (line[i] <= ' ') return true;
+            }
+            if (!reread()) return false;
+        }
+    }
+};
+
+//fast Output by ei1333
+/**
+ * @brief Printer(高速出力)
+ */
+struct Printer {
+public:
+  explicit Printer(FILE *fp) : fp(fp) {}
+
+  ~Printer() { flush(); }
+
+  template< bool f = false, typename T, typename... E >
+  void write(const T &t, const E &... e) {
+    if(f) write_single(' ');
+    write_single(t);
+    write< true >(e...);
+  }
+
+  template< typename... T >
+  void writeln(const T &...t) {
+    write(t...);
+    write_single('\n');
+  }
+
+  void flush() {
+    fwrite(line, 1, st - line, fp);
+    st = line;
+  }
+
+private:
+  FILE *fp = nullptr;
+  static constexpr size_t line_size = 1 << 16;
+  static constexpr size_t int_digits = 20;
+  char line[line_size + 1] = {};
+  char small[32] = {};
+  char *st = line;
+
+  template< bool f = false >
+  void write() {}
+
+  void write_single(const char &t) {
+    if(st + 1 >= line + line_size) flush();
+    *st++ = t;
+  }
+
+  template< typename T, enable_if_t< is_integral< T >::value, int > = 0 >
+  void write_single(T s) {
+    if(st + int_digits >= line + line_size) flush();
+    if(s == 0) {
+      write_single('0');
+      return;
+    }
+    if(s < 0) {
+      write_single('-');
+      s = -s;
+    }
+    char *mp = small + sizeof(small);
+    typename make_unsigned< T >::type y = s;
+    size_t len = 0;
+    while(y > 0) {
+      *--mp = y % 10 + '0';
+      y /= 10;
+      ++len;
+    }
+    memmove(st, mp, len);
+    st += len;
+  }
+
+  void write_single(const string &s) {
+    for(auto &c : s) write_single(c);
+  }
+
+  void write_single(const char *s) {
+    while(*s != 0) write_single(*s++);
+  }
+
+  template< typename T >
+  void write_single(const vector< T > &s) {
+    for(size_t i = 0; i < s.size(); i++) {
+      if(i) write_single(' ');
+      write_single(s[i]);
+    }
+  }
+};
+
+}; //namespace fastio
+using mint=Fp<998244353>;
+int main(){
+  fastio::Scanner sc(stdio);
+  fastio::Printer pr(stdout);
+}

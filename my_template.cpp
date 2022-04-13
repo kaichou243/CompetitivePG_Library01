cd8ddbfa8dbb6550ae07729ba18cbab6529f596b
kaichou243 <83384006+kaichou243@users.noreply.github.com>
2022-04-13 15:42:45 +0900

    Update my_template.cpp

--- my_template.cpp
+++ my_template.cpp
@@ -1971,6 +1971,47 @@ struct CompressedWaveletMatrix {
     return ret == -1 ? T(-1) : ys[ret];
   }
 };
+struct MST{
+  struct MSTEdge{
+    ll u,v;
+    ll cost;
+    bool used;
+    bool operator<(const MSTEdge& o) const {
+      return cost < o.cost;
+    }
+  };
+  int n;
+  vector<MSTEdge> edges;
+  MST(int sz) : n(sz), edges(sz) {}
+  void add_edge(int u,int v,ll c){
+    edges.push_back({u,v,c,false});
+  }
+  ll kruskal(){
+    UnionFind uf(n);
+    sort(all(edges));
+    ll min_cost=0;
+    for(int i=0;i<sz(edges);i++){
+      auto& [u,v,c,used]=edges[i];
+      if(!uf.same(u,v)){
+        uf.unite(u,v);
+        used=true;
+        min_cost+=c;
+      }
+    }
+    return min_cost;
+  }
+  Graph Tree(){
+    kruskal();
+    Graph G(n);
+    for(int i=0;i<sz(edges);i++){
+      if(edges[i].used){
+        G[edges[i].u].push_back({edges[i].v,edges[i].cost});
+        G[edges[i].v].push_back({edges[i].u,edges[i].cost});
+      }
+    }
+    return G;
+  }
+};
 template<typename T>
 vector<T> shortest_path_faster_algorithm(const Graph &G, int s) {
   vector<T> dist(G.size(), INF);

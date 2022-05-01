44bff8682c9d03edd6f66be970c464a378e4ae9c
kaichou243 <83384006+kaichou243@users.noreply.github.com>
2022-05-01 20:55:47 +0900

    Update my_template.cpp

--- my_template.cpp
+++ my_template.cpp
@@ -2091,6 +2091,33 @@ template <typename mint> FPS<mint> interpolate(const vector<mint> &x,const vecto
   subproduct_tree<mint> tree(x);
   return tree.interpolate(y);
 }
+template <typename mint> struct PartitionNumber{
+    using poly=FPS<mint>;
+    poly f;
+    PartitionNumber(int maxN,vector<int> st) : f(maxN+1){
+        vector<mint> invs(maxN+1);
+        for(int i=1;i<=maxN;i++) invs[i]=modinv(mint(i));
+        for(auto& e:st){
+            for(int j=1;j<=maxN/e;j++){
+                f[e*j]+=invs[j];
+            }
+        }
+        f=exp(f);
+    }
+    PartitionNumber(int maxN) : f(maxN+1){
+        vector<mint> invs(maxN+1);
+        for(int i=1;i<=maxN;i++) invs[i]=modinv(mint(i));
+        for(int i=1;i<=maxN;i++){
+            for(int j=1;j<=maxN/i;j++){
+                f[i*j]+=invs[j];
+            }
+        }
+        f=exp(f);
+    }
+    mint operator()(int n){
+        return f[n];
+    }
+};
 //fast Input by yosupo
 #include <unistd.h>
 #include <algorithm>
